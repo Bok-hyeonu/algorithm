9af57d1e3a55431fe8cb4a59642ee484b9c9b0e2
복현우 <127809940+Bok-hyeonu@users.noreply.github.com>
2024-02-01 17:20:13 +0900

    [Bronze II] Title: 방 배정, Time: 84 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/13300. 방 배정/방 배정.py
+++ 백준/Bronze/13300. 방 배정/방 배정.py
@@ -2,13 +2,13 @@ N, K = map(int, input().split()) # N : 학생 수, K : 방 수
 stu_list = [[0]*2 for _ in range(6)]
 for i in range(N):
     S, Y = map(int, input().split()) # S : 여 0 남 1, Y : 학년
-    stu_list[Y-1][S] += 1
+    stu_list[Y-1][S] += 1 # 해당 학년 성별 인원 + 1
 
-total = 0
-for i in range(6):
-    for j in range(2):
-        total += stu_list[i][j]//2
-        if stu_list[i][j] % 2 == 1:
+total = 0 # 전체 방 수
+for i in range(6): # 학년
+    for j in range(2): # 성별
+        total += stu_list[i][j]//K # 정원이 차는 방
+        if stu_list[i][j] % K != 0: # 정원이 안 차는 방
             total += 1
 
-print(total)
+print(total) 
