52a157559dd2110a551d721594d48be20edcbae0
복현우 <127809940+Bok-hyeonu@users.noreply.github.com>
2024-02-03 22:58:51 +0900

    Update 수 정렬하기 2.py

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -1,6 +1,7 @@
 import sys
-N = int(sys.stdin.readline())
-nums = [int(sys.stdin.readline()) for _ in range(N)]
-nums.sort()
-for i in range(N):
+N = int(sys.stdin.readline()) # N개의 수
+# N개의 수를 순서대로 입력받아 리스트에 추가
+nums = [int(sys.stdin.readline()) for _ in range(N)] 
+nums.sort() # 오름차순 정렬
+for i in range(N): # N개의 수 순서대로 출력
     print(nums[i])
