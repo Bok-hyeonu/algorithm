b224b2a886bddc652e27d1629b2b4c61aa68f891
복현우 <127809940+Bok-hyeonu@users.noreply.github.com>
2024-02-10 15:48:16 +0900

    [Bronze I] Title: 최소공배수, Time: 136 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.py
+++ 백준/Bronze/1934. 최소공배수/최소공배수.py
@@ -1,21 +1,7 @@
-def findgcd(num1, num2): # 최대공약수를 찾는 함수
-    N = num1
-    if num2%N==0:
-        return N
-    else:
-        N //= 2
-        while N>=1:
-            if num2%N==0 and num1%N==0:
-                break
-            else:
-                N -= 1
-        return N
-
-T = int(input()) # 테스트 케이스
+T=int(input())
 for i in range(T):
-    A, B = map(int, input().split())
-    if A > B:
-        A, B = B, A
-    G = findgcd(A, B)
-    print(A*B//G) # 최소 공배수는 두 수의 곱 / 두 수의 최대공약수
-    
+    a,b=map(int,input().split())
+    a1,b1=a,b
+    while a%b!=0: # 최대공약수 알고리즘
+        a,b=b,a%b # 나머지로 나눔
+    print(a1*b1//b)
