8f741b556899084cbaee30e7976da89fd748b6f9
복현우 <127809940+Bok-hyeonu@users.noreply.github.com>
2024-02-21 22:20:43 +0900

    Update 나이순 정렬.py

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -1,6 +1,6 @@
 import sys
 infos = [[] for _ in range(201)]            # 200살까지의 정보를 담을 이중 배열
-N = int(input())                            # 입력값의 수
+N = int(sys.stdin.readline())               # 입력값의 수
 for i in range(N):                          # N쌍을 입력 받음
     info = sys.stdin.readline().split()
     infos[int(info[0])].append(info[1])     # 나이를 인덱스로 하는 이중 배열에 정보 저장
