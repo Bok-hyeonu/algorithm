c5bc53b8bb698bdd2cc96d07206e9a26eb3deac0
복현우 <127809940+Bok-hyeonu@users.noreply.github.com>
2024-03-03 15:41:50 +0900

    [D4] Title: 준환이의 양팔저울, Time: 3,995 ms, Memory: 65,972 KB -BaekjoonHub

--- SWEA/D4/3234. 준환이의 양팔저울/준환이의 양팔저울.py
+++ SWEA/D4/3234. 준환이의 양팔저울/준환이의 양팔저울.py
@@ -13,20 +13,14 @@ def f(i, k, l, r, total):
     # 모든 무게를 다 올려도 우측이 더 큰 경우
     elif total + r <= l:
         cnt += fact[k-i-1] * (2**(k-i))
-    # 오른쪽에 올릴 필요가 없는 경우
-    elif l == r:
-        for j in range(N):
-            if used[j] == 0:
-                used[j] = 1
-                f(i+1, k, l+weis[j], r, total - weis[j])
-                used[j] = 0              
     else:
         for j in range(N):
             # 사용하지 않은 무게추라면
             if used[j] == 0:
                 used[j] = 1
-                for s in range(2): # 양쪽 모두 달아봄(0왼, 1오)
-                    f(i+1, k, l + weis[j]*((s+1)%2), r + weis[j]*(s%2), total - weis[j])
+                f(i + 1, k, l + weis[j], r, total - weis[j])
+                if l >= r + weis[j]: # 우측에 무게를 올릴 수 있는 경우
+                    f(i+1, k, l, r + weis[j], total - weis[j])
                 used[j] = 0
 
 # SWEA 3234. 준환이의 양팔저울
