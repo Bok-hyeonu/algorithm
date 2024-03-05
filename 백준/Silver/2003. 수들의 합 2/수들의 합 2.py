6ccbdea78227f8141fd08117c709249fcafa3da5
복현우 <127809940+Bok-hyeonu@users.noreply.github.com>
2024-03-05 16:26:37 +0900

    [Silver IV] Title: 수들의 합 2, Time: 44 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.py
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.py
@@ -1,28 +1,25 @@
 import sys
 # N : 수열의 길이, M : 목표 합
 N, M = map(int, sys.stdin.readline().split()) 
-arr = [0]
-arr += list(map(int, sys.stdin.readline().split()))
-
-# 해당 인덱스까지의 총합 계산 및 탐색 시작 인덱스 계산
-st = None               # 탐색 시작 인덱스, 이 인덱스 이전까지의 합으론 M에 도달 불가
-for i in range(1, N+1):
-    arr[i] += arr[i-1]
-    if st == None:
-        if arr[i] >= M:
-            st = i
-cnt = 0                 # 경우의 수
-if st:                  # M이 모든 수열의 합보다 큰 경우 탐색 필요 없음
-    # i번째 수부터 j번째 수까지의 합은 
-    # j번째 수까지의 합에서 i-1번째 수까지의 합을 빼는 것과 같음
-    # 이 경우는 경우의 수를 구하는 문제이므로 배열의 가장 앞에 0을 추가할 경우
-    # 인덱스를 고려할 필요가 없음
-    for i in range(N):
-        for j in range(max(st,i+1), N+1):
-            # i번째 수부터 j번째 수까지의 합이 목표 합을 넘은 경우
-            # j를 늘릴 필요가 없음
-            if arr[j] - arr[i] > M: break
-            # 값이 같은 경우 경우의 수 증가
-            elif arr[j] - arr[i] == M: cnt += 1
-
-sys.stdout.write(f'{cnt}\n')
+arr = list(map(int, sys.stdin.readline().split()))
+# 투 포인터
+st = ed = cnt = 0
+total = 0
+while True:
+    # 총 합이 목표보다 작은 경우
+    if total < M:
+        # 인덱스를 늘릴 수 없는 경우 종료
+        if ed == N:
+            break
+        # ed가 가리키는 인덱스를 증가
+        total += arr[ed]
+        ed += 1 # ed의 인덱스 증가
+    else:
+        # 목표와 같은 경우
+        if total == M:
+            # 개수 상승
+            cnt += 1
+        # st 인덱스 값을 빼주고 1 상승
+        total -= arr[st]
+        st += 1
+print(cnt)
