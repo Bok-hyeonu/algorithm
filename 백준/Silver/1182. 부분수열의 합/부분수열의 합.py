c3e3c2ff113913916fbe6684e5202a9e1c60569a
복현우 <127809940+Bok-hyeonu@users.noreply.github.com>
2024-03-24 21:26:54 +0900

    [Silver II] Title: 부분수열의 합, Time: 252 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,14 +1,19 @@
-# 부분 수열의 합
+def f(i, total):
+    global cnt
+    # 모든 경우의 수를 탐색했다면
+    if i == N:
+        # 목표 수인지 탐색
+        if total == S:
+            cnt += 1
+        return
+    # 아직 탐색할 것이 남은 경우
+    else:
+        f(i+1, total)               # 빼고 할건지
+        f(i+1, total+numbers[i])    # 넣고 할건지
+
 N, S = map(int, input().split())            # 정수의 개수, 목표 수
 numbers = list(map(int, input().split()))   # 수열
-cnt = 0                         # 경우의 수
-for tar in range(1, 1<<N):
-    total = 0                   # 수열의 부분합
-    for i in range(N):
-        if tar & 0x1:
-            total += numbers[i] 
-        tar >>= 1
-    if total == S:              # 부분합이 목표 수이면 
-        cnt += 1                # 경우의 수 증가
-
+if S == 0: cnt = -1                         # 공집합의 부분수열이
+else: cnt = 0                               # 0인 경우 제외
+f(0, 0)
 print(cnt)
