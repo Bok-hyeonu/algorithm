04a4aa07b084aa199deddc4712b66f7b124abd9d
복현우 <127809940+Bok-hyeonu@users.noreply.github.com>
2024-09-21 19:35:53 +0900

    [Silver IV] Title: 인사성 밝은 곰곰이, Time: 72 ms, Memory: 42388 KB -BaekjoonHub

---
+++ 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.py
@@ -0,0 +1,18 @@
+import sys 
+
+input = sys.stdin.readline
+
+n = int(input())
+gom = set()
+cnt = 0
+
+for _ in range(n):
+    user = input().strip()
+    if user == 'ENTER':
+        cnt += len(gom)
+        gom = set()
+    else:
+        gom.add(user)
+
+cnt += len(gom)
+print(cnt)
